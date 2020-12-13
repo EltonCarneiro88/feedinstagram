7151bfde9e90cb84882f70142e0d1e90035b6e30
EltonCarneiro88 <71104517+EltonCarneiro88@users.noreply.github.com>
2020-12-12 23:49:49 -0300

    Update tela de curtidas

--- src/pages/FeedScreens/Likes.js
+++ src/pages/FeedScreens/Likes.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import LikeList from '../../../src/components/LikeList';
 
-export default function Likes(){
-    return (
-            null
-    );
+export default class Likes extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          item: [],
+        };
+    }
+    
+    componentDidMount() {
+    axios.get(`https://5fa103ace21bab0016dfd97e.mockapi.io/api/v1/feed?page=1&limit=4`)
+    .then(res => {
+        const item = res.data;
+        this.setState({ item });
+    })
+    }
+    render() {
+        return (
+        <View>
+            <LikeList
+                item={this.state.item}
+            />
+        </View>
+        );
+    }
 }
